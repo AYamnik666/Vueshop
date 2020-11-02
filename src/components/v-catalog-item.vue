aa0d463ba77078a4d2def0f9b3f8318a3c0aa16e
Stacy, фигачу на сервере <anastasiy.yamnik@gmail.com>
2020-11-02 23:33:55 +0600

    start project

---
+++ src/components/v-catalog-item.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="v-catalogItem">
+    <h1>CatalogItem</h1>
+        <p>Price</p>
+    <button>Add to cart</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'v-catalog-item'
+}
+</script>
+<style>
+
+</style>
---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="v-main-wrapper">{{title}}
+    <v-catalog />
+    <v-cart />
+  </div>
+</template>
+<script>
+import vCatalog from './v-catalog';
+import vCart from './v-cart'
+export default {
+  components: { vCatalog , vCart},
+  name: "v-main-wrapper",
+  data(){
+    return{
+      title: 'Main wrapper'
+    }
+  },
+
+
+}
+</script>
+<style>
+.v-main-wrapper{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 900px;
+  margin: 0 auto;
+}
+</style>
